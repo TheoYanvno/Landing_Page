b14e50dedf3f398e8753f9b5603553d1c8818b28
TheoYanvno <TheoYanvno@gmail.com>
2023-01-12 15:54:43 +0000

    inital push

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer () {
+    return (
+        <footer className="bg-dark">
+        <p className="mx-auto text-white "> Copyright <i class="fa-regular fa-copyright"></i> Your Website 2022</p>
+     </footer>
+    )
+};
+export default Footer;
