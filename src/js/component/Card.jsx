1e3ad4fc6d6533cb051b7f626e129c05d3e92c24
TheoYanvno <116613626+TheoYanvno@users.noreply.github.com>
2023-02-08 23:57:41 -0500

    Update Card.jsx

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function Card () {
         return (
-        <div className="card" style="width: 18rem;">
+        <div className="card" style={{width: "18rem"}}>
 					<img src="..." class="card-img-top" alt="..."/>
 					<div className="card-body">
 						<h5 className="card-title">Card title</h5>
