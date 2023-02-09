192b3f1dcc131bcfb3f9d24671f28ecaa1f3c302
TheoYanvno <116613626+TheoYanvno@users.noreply.github.com>
2023-02-08 23:59:02 -0500

    Update Footer.jsx

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function Footer () {
     return (
-        <footer className="bg-dark">
+        <footer className="bg-dark position-absolute bottom-0 w-100">
         <p className="mx-auto text-white "> Copyright <i class="fa-regular fa-copyright"></i> Your Website 2022</p>
      </footer>
     )
